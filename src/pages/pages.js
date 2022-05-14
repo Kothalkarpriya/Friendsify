4b58a93402bac8d9635391d410657c99ef35fe0a
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-05-14 20:53:52 +0530

    feat-components and pages added

---
+++ src/pages/pages.js
@@ -0,0 +1,19 @@
+import Home from "./Home/Home";
+import Profile from "./Profile/Profile";
+import Notifications from "./Notification/Notifications";
+import LandingPage from "./Landing/LandingPage";
+import Explore from "./Explore/Explore";
+import BookMarks from "./Bookmarks/BooksMarks";
+import Login from "./Auth/Login";
+import SignIn from "./Auth/SignIn";
+
+export {
+    Home,
+    Profile,
+    Notifications,
+    LandingPage,
+    Explore,
+    BookMarks,
+    Login,
+    SignIn
+};
