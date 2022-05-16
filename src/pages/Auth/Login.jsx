658d8dc587c4e29c48dd472015d012d062e05d1d
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-05-16 22:20:57 +0530

    feat-login and signup pages added

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,36 +1,46 @@
+import React from "react";
 import "../../assests/styles/auth.css";
 import "../../assests/styles/leftsidebar.css";
 import { AiOutlineRight } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
     <main>
-      <form>
+      <section className="login">
         <header>Login</header>
+        <form>
+          <div className="form-block">
+            <label className="label" for="#input-email">
+              Email Address
+            </label>
+            <input id="input-email" type="email" placeholder="username@gmail.com" required/>
+          </div>
+          <div className="form-block">
+            <label className="label" for="#input-pass">
+              Password
+            </label>
+            <input type="password" id="input-pass" placeholder="**********" required/>
+          </div>
+          <div className="form-block-check">
+            <div className="input">
+              <input type="checkbox" required/>
+              Remember Me
+            </div>
+            <div className="btn-pass">
+              <Link className="Link" to="/">Forgot your Password?</Link>
+            </div>
+          </div>
+          <div className="form-block">
+            <button className="btn btn-left">Login</button>
+          </div>
+        </form>
         <div className="form-block">
-          <lable className="label">
-            Email Address
-            <input placeholder="username@gmail.com" />
-          </lable>
+          <Link className="Link" to="/signin">
+            Create New Account <AiOutlineRight />
+          </Link>
         </div>
-        <div className="form-block">
-          <label className="label">
-            Password
-            <input type="password" placeholder="**********" />
-          </label>
-        </div>
-        <div className="form-block">
-          <label className="label">
-            <input type="checkbox" />
-            Remember Me
-          </label>
-          <button>Forgot your Password?</button>
-        </div>
-        <button className="btn btn-left">Login</button>
-        <button>
-          Create New Account <AiOutlineRight />
-        </button>
-      </form>
+      </section>
     </main>
   );
 }
--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -1,9 +1,80 @@
+import React from "react";
 import "../../assests/styles/auth.css";
+import { Link } from "react-router-dom";
+import { AiOutlineRight } from "react-icons/ai";
 
-export default function SignIn(){
-    return(
-        <main>
-            SignIn page
-        </main>
-    )
+export default function SignIn() {
+  return (
+    <main>
+      <section className="signin">
+        <header>Signup</header>
+        <form>
+          <div className="form-block-name">
+            <div className="form-block">
+              <label className="label" for="#input-firstName" >
+                First Name
+              </label>
+              <input type="text" id="input-firstName" placeholder="First Name" required />
+            </div>
+            <div className="form-block">
+              <label className="label" for="#input-lastName">
+                Last Name
+              </label>
+              <input type="text" id="input-lastName" placeholder="Last Name" required />
+            </div>
+          </div>
+          <div className="form-block">
+            <label className="label" for="#input-email">
+              Email Address
+            </label>
+            <input
+              id="input-email"
+              type="email"
+              placeholder="username@gmail.com"
+              required
+            />
+          </div>
+          <div className="form-block">
+            <label className="label" for="#input-pass">
+              Password
+            </label>
+            <input
+              type="password"
+              id="input-pass"
+              placeholder="**********"
+              required
+            />
+          </div>
+          <div className="form-block">
+            <label className="label" for="#input-pass-again">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="input-pass-again"
+              placeholder="**********"
+              required
+            />
+          </div>
+          <div className="form-block-check">
+            <div className="input">
+              <input type="checkbox" required />
+              Show Password
+            </div>
+            <div className="input">
+              <input type="checkbox" required />I accept to Signin
+            </div>
+          </div>
+          <div className="form-block">
+            <button className="btn btn-left">Login</button>
+          </div>
+        </form>
+        <div className="form-block">
+          <Link className="Link" to="/signin">
+            Already have an Account <AiOutlineRight />
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 }
